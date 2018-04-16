032493510042c829280081d2aaa28a33c318159c
daniel <dannydaniel180@gmail.com>
2018-04-16 09:31:37 -0700

    Cambie el HTML para el sitio.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from . import views
+
+
 # Create your views here.
-urlpatterns = [
-    url(r'^$', views.pot_list),
-]
+def post_list(request):
+     return render(request, 'blog/post_list.html',{})
